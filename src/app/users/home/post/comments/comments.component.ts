e997c32c1ceae44b5d72c76e787e55dc2a86c024
Sidney <sidneyrnovaes@gmail.com>
2018-09-16 15:37:40 -0300

    componente de comentários

---
+++ src/app/users/home/post/comments/comments.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.scss']
+})
+export class CommentsComponent implements OnInit {
+
+  private comments = [
+    {
+      text: "Comentário teste 1!",
+      photo: "https://www.yourfirstpatient.com/assets/default-user-avatar-thumbnail@2x-ad6390912469759cda3106088905fa5bfbadc41532fbaa28237209b1aa976fc9.png",
+      answers: [
+        {comment: 'Teste 1'},{comment: 'Teste 2'}
+      ]
+    },
+    {
+      text: "Comentário teste 2!",
+      photo: "https://www.yourfirstpatient.com/assets/default-user-avatar-thumbnail@2x-ad6390912469759cda3106088905fa5bfbadc41532fbaa28237209b1aa976fc9.png",
+      answers: []
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  newComment(comment) {
+
+    this.comments.push(comment);
+
+  }
+
+  
+
+}
